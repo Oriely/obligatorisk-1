3a35c89dd02a5240cc863be60fe5f7800fbed330
Oriely <orielygaming@protonmail.com>
2020-11-09 00:12:21 +0000

    finish 2

--- primary.js
+++ primary.js
@@ -1,144 +1,101 @@
-let realdate = "30.02.2019";
+function _dateLength(date) { return (date.length === 10); }
 
-// check string length of "date" variable
-function _dateLength(date) {
-    return (date.length === 10);
-}
+function _dots(date) { return (date.charAt(2) == '.' && date.charAt(5) == '.'); }
 
-// check if punctionation mark is in correct position in "date" variable
-function _dots(date) {
+function _leapYear(year) { return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0); }
+
+function _yearReal(date) { return (date.split('.')[2].length == 4); }
+
+function _years(date) { return (date.split('.')[2].length === 4 && date.split('.')[2] >= '0000' && date.split('.')[2] <= '9999'); }
+
+function _monthReal(date) { return (date.split('.')[1].length === 2 && date.split('.')[1] >= '01' && date.split('.')[1] <= '12'); }
+
+function _days(date) {
+    let day = date.split('.')[0];
+    let mth = date.split('.')[1];
+    let yr = date.split('.')[2];
     return (
-        date.charAt(2) == '.' && date.charAt(5) == '.');
-}
-
-//check if current year in "date" variable is a leap year
-function _leapYear(year) {
-    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
-}
-
-function _yearReal(date) {
-    return (date.split('.')[2].length == 4);
-}
-
-function _februaryCheck(date) {
-    return (
-        parseInt(date.split('.')[0]) <= 28 &&
-        parseInt(date.split('.')[1]) === 2 ||
-
-        parseInt(date.split('.')[0]) <= 29 &&
-        parseInt(date.split('.')[1]) === 2 &&
-        _leapYear(parseInt(date.split('.')[2])) === true
+        day <= '28' && mth <= '12' ||
+        day <= '31' && mth == '01' ||
+        day <= '31' && mth == '03' ||
+        day <= '31' && mth >= '05' && mth <= '12' ||
+        day <= '30' && mth == '04' ||
+        day <= '28' && mth == '02' ||
+        day <= '29' && mth == '02' && _leapYear(yr) == true
     );
-}
-
-function _checkMonths(date) {
-    return (
-        _februaryCheck(date) == true ||
-        parseInt(date.split('.')[0]) <= 30 &&
-        parseInt(date.split('.')[1]) === 4 ||
-
-        parseInt(date.split('.')[0]) <= 31 &&
-        parseInt(date.split('.')[1]) === 1 ||
-
-        parseInt(date.split('.')[0]) <= 31 &&
-        parseInt(date.split('.')[1]) === 3 ||
-
-        parseInt(date.split('.')[0]) <= 31 &&
-        parseInt(date.split('.')[1]) === 5 ||
-
-        parseInt(date.split('.')[0]) <= 31 &&
-        parseInt(date.split('.')[1]) === 6 ||
-
-        parseInt(date.split('.')[0]) <= 31 &&
-        parseInt(date.split('.')[1]) === 7 ||
-
-        parseInt(date.split('.')[0]) <= 31 &&
-        parseInt(date.split('.')[1]) === 8 ||
-
-        parseInt(date.split('.')[0]) <= 31 &&
-        parseInt(date.split('.')[1]) === 9 ||
-
-        parseInt(date.split('.')[0]) <= 31 &&
-        parseInt(date.split('.')[1]) === 10 ||
-
-        parseInt(date.split('.')[0]) <= 31 &&
-        parseInt(date.split('.')[1]) === 11 ||
-
-        parseInt(date.split('.')[0]) <= 31 &&
-        parseInt(date.split('.')[1]) === 12
-    );
-}
-
-function _monthsReal(date) {
-    return (parseInt(date.split('.')[1]) <= 12);
 }
 
 // do multpiple checks to check if the whole string of the date variable is
 // a "correct" date 
 function _isDateValid(date) {
-    return (_dateLength(date) && _dots(date) && _yearReal(date) && _checkMonths(date) && _monthsReal(date));
+    return (_dateLength(date) && _dots(date) && _yearReal(date) && _monthReal(date) && _days(date));
 }
-
 
 QUnit.module('add', function() {
     QUnit.test('date does not have length of 10', function(assert) {
-        let actual = "11.12.199";
+        let actual = _isDateValid("11.12.199");
         let expected = false;
-        assert.equal(_isDateValid(actual), expected);
+        assert.equal(actual, expected);
 
     });
     QUnit.test('punctionation is not in correct position', function(assert) {
-        let actual = "11.121.996";
+        let actual = _isDateValid("11.121.996");
         let expected = false;
-        assert.equal(_isDateValid(actual), expected);
+        assert.equal(actual, expected);
 
     });
     QUnit.test('year does not have a length of 4 ', function(assert) {
-        let actual = "11.12.199";
+        let actual = _isDateValid("11.12.199");
         let expected = false;
-        assert.equal(_isDateValid(actual), expected);
+        assert.equal(actual, expected);
 
     });
     QUnit.test('month doesn not have two integers', function(assert) {
-        let actual = "11.12.1996";
-        let expected = true;
-        assert.equal(_isDateValid(actual), expected);
+        let actual = _isDateValid("11.1.1996");
+        let expected = false;
+        assert.equal(actual, expected);
+
+    });
+    QUnit.test('month is not 2 numbers', function(assert) {
+        let actual = _isDateValid("11.2.1996");
+        let expected = false;
+        assert.equal(actual, expected);
 
     });
     QUnit.test('month outside of 1-12 ', function(assert) {
-        let actual = "11.15.1996";
+        let actual = _isDateValid("11.15.1996");
         let expected = false;
-        assert.equal(_isDateValid(actual), expected);
+        assert.equal(actual, expected);
 
     });
     QUnit.test('if day is higher than 31 function', function(assert) {
-        let actual = "32.12.1996";
+        let actual = _isDateValid("32.12.1996");
         let expected = false;
-        assert.equal(_isDateValid(actual), expected);
+        assert.equal(actual, expected);
 
     });
     QUnit.test('day is 31st for a month without 31 days.', function(assert) {
-        let actual = "31.04.1996"
+        let actual = _isDateValid("31.04.1996")
         let expected = false;
-        assert.equal(_isDateValid(actual), expected);
+        assert.equal(actual, expected);
 
     });
     QUnit.test('day is 30th and month is february', function(assert) {
-        let actual = "30.02.2020"
+        let actual = _isDateValid("30.02.2020")
         let expected = false;
-        assert.equal(_isDateValid(actual), expected);
+        assert.equal(actual, expected);
 
     });
     QUnit.test('day is 29th, its february and its not a leap year', function(assert) {
-        let actual = "29.02.2019"
+        let actual = _isDateValid("29.02.2019");
         let expected = false;
-        assert.equal(_isDateValid(actual), expected);
+        assert.equal(actual, expected);
 
     });
     QUnit.test('If its leap year and its february 29th', function(assert) {
-        let actual = "29.02.2020"
+        let actual = _isDateValid("29.02.2020");
         let expected = true;
-        assert.equal(_isDateValid(actual), expected);
+        assert.equal(actual, expected);
 
     });
 
